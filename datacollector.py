a3de617dac0566a8f4a09cff7cf9460359d211cb
trainorpj <trainorpj@gmail.com>
2016-01-05 23:59:52 -0500

    Working

--- datacollector.py
+++ datacollector.py
@@ -1,15 +1,21 @@
-from datatools import listBuilder, filmEnter
+from datatools import listBuilder, filmEnter, dictAdd
 
-directors=["Kurosawa, Akira", "Eastwood, Clint"]
+directors=["Chaplin, Charles", "Eastwood, Clint", "Kurosawa, Akira"]
 json_master=[]
 
 
 dirdum=[]
 produm=[]
+edidum=[]
+wridum=[]
+actdum=[]
+
 
 listBuilder(dirdum, directors, 'director')
 listBuilder(produm, directors, 'producer')
-
+listBuilder(edidum, directors, 'editor')
+listBuilder(wridum, directors, 'writer')
+listBuilder(actdum, directors, 'actor')
 
 
 
@@ -20,14 +26,26 @@ for elmt in dirdum:
         entry = {"name": 0, "nofilms": 0, "films": []}  #makes new dict
         entry['name'] = elmt                    #puts director name in dict
     else:
-        filmEnter(elmt, 'director', dirdum, entry, json_master)
-        '''                                       #makes film entry
+        filmEnter(elmt, 'director', dirdum, entry, json_master, True)
+
+dictAdd(actdum, json_master, 'actor')
+dictAdd(produm, json_master, 'producer')
+dictAdd(edidum, json_master, 'editor')
+dictAdd(wridum, json_master, 'writer')
+
+for elmt in json_master:
+    count = len(elmt['films'])
+    elmt['nofilms'] = count
+    print elmt['name']
+
+
+'''                                       #makes film entry
         filmentry = {"title":elmt[0], "year":elmt[1], "credits":['director']}
         entry["films"].append(filmentry)
         next_index = dirdum.index(elmt) +1
         if type(dirdum[next_index]) == str:     #sees when another director comes up
             json_master.append(entry)           #adds dict to mast director list
-        '''
+    print entry, '\n'
 
 cont = False
 dirindex = 0
@@ -46,7 +64,7 @@ for elmt in produm:
                 break
     if cont == True:
         for film in json_master[dirindex]["films"]:
-            print elmt , film["title"], elmt[0] == film["title"]
+           # print elmt , film["title"], elmt[0] == film["title"]
             if elmt[0] == film["title"]:
                 if 'producer' in film["credits"]:
                     break
@@ -62,20 +80,6 @@ for elmt in produm:
     if type(produm[next_index]) == str:
         cont = False
 
-
-'''
-            else:
-                filmEnter(elmt, 'producer', produm, json_master[dirindex], json_master)
-    next_index = produm.index(elmt) +1
-    if type(produm[next_index]) == str:
-        cont = False
-
-
-    if type(elmt) == list:
-        for item in json_master:
-            for film in item['films']:
-                if elmt[0] == film['title']:
-                    film['credits'].append('producer')
     '''
 
 
--- datatools.py
+++ datatools.py
@@ -12,35 +12,122 @@ def listBuilder(poplst, dirlist, role):
         for line in fileobject:     #goes line by line in file
             for elmt in dirlist:    #loops through names of directors
                 if elmt in line:    #locates a director
-                    name_sep = line.split("\t\t")       #split director name
+                    if '(I)\t' in line:
+                        name_sep = line.split('(I)\t')
+                    if '\t\t' in line:
+                        name_sep = line.split("\t\t")     #split director name
+                    else:
+                        name_sep = line.split('\t')
+                    #print name_sep
                     title_sep = name_sep[1].split("(",1) #split title
                     poplst.append(name_sep[0])          #add director name
+                    cont = True
+                    skip = checkYear(title_sep)         #check if first
+                    if skip == True:                    #   title is trash
+                        continue
                     dummy=[title_sep[0][:-1], int(title_sep[1][:4])]
                     poplst.append(dummy)               #add [first title, year]
-                    cont = True
             if cont== True:         #works for lines other than the first one
                 if line == '\n':
                     cont = False    #skips line b/c it's the end of a director
                 else:
                     title_sep = line[:-1].split("\t\t\t") #split title
-                    if len(title_sep) == 1:
-                        continue
-                    if "TV" in title_sep[1]:
-                        continue
-                    if "????" in title_sep[1]:
+                    skip = checkYear(title_sep)
+                    if skip == True:
                         continue
                     year_sep = title_sep[1].split("(",1)
+                    #print year_sep
                     dumentry = [year_sep[0][:-1], int(year_sep[1][:4])]
                     poplst.append(dumentry)             #add [title, year]
     poplst.append('-')      #'-' indicates end of list
 
+def checkYear(title_sep):
+    if len(title_sep) == 1:
+        return True
+    if "TV" in title_sep[1]:
+        return True
+    if "Himself" in title_sep[1]:
+        return True
+    if "????" in title_sep[1]:
+        return True
+    if "#" in title_sep[1]:
+        return True
+    if"archive footage" in title_sep[1]:
+        return True
+    else:
+        return False
 
-def filmEnter(lst, credit, dum, entry, master):
+
+def filmEnter(lst, credit, dum, entry, master, tf):
     filmentry = {"title": lst[0], "year": lst[1], "credits": [credit]}
     entry["films"].append(filmentry)
-    next_index = dum.index(lst) +1
-    if type(dum[next_index]) == str:
-        master.append(entry)
+    if tf == True:
+        next_index = dum.index(lst) +1
+        if type(dum[next_index]) == str:
+            master.append(entry)
+
+
+def dictAdd(dum, master, role):
+    cont = False
+    dirindex = 0
+    tempfilms = 0
+    for elmt in dum:
+        failno=0
+        if elmt == '-':     #end of list
+            continue
+        if type(elmt) == str:   #looks for name of director
+            cont= True
+            for item in master: #run through each entry in dict
+                if item["name"] == elmt:    #looks for director match
+                    dirindex = master.index(item)
+                    tempfilms = len(master[dirindex]["films"])
+                    templst=[]
+                    for titles in master[dirindex]['films']:
+                        templst.append(titles['title'])
+                    print templst
+                    #cont = True
+                    break
+        if cont == True:
+            if len(elmt[0]) == 1:
+                continue
+            if elmt[0] in templst:
+                for film in master[dirindex]["films"]:
+                    if elmt[0] == film['title']:
+                        if role in film['credits']:
+                            break
+                        film['credits'].append(role)
+                        break
+               # print elmt[0], master[dirindex]['name']
+               # find = master[dirindex]["films"].index(elmt[0])
+               # master[dirindex]["films"][find]['credits'].append('role')
+            else:
+                filmEnter(elmt, role, dum, master[dirindex], master, False)
+
+
+
+            '''
+            for film in master[dirindex]["films"]: #runs through directors films
+                if elmt[0] == film["title"]:
+                    if role in film["credits"]:
+                        break
+                    film['credits'].append(role)
+                    #print elmt[0], film['credits']
+                    failno = 0
+                    break
+                failno = failno +1
+
+
+        print "fail:", failno
+        print "total:", tempfilms
+        print '---------------------------------'
+        if failno == tempfilms:
+            #print elmt, "new film", role
+            print '--------------------------------------',role
+            filmEnter(elmt, role, dum, master[dirindex], master)
+        next_index = dum.index(elmt)+1
+        if type(dum[next_index]) == str:
+            cont = False
+'''
 
 
 
